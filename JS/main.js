e799e388a717699efa5126c2ca3d6b4554445f25
Juan Aguilar <juan.aguilarl@unmsm.edu.pe>
2024-11-03 23:46:02 -0500

    Estilos del mapa, funciona en teléfonos

--- JS/main.js
+++
@@ -1,8 +0,0 @@
-function infoZonasRiesgo(){
-    document.getElementById("rutasSegurasMap").style.display= "none";
-    document.getElementById("zonasRiesgoMap").style.display= "block";
-}
-function infoRutasSeguras(){
-    document.getElementById("rutasSegurasMap").style.display= "block";
-    document.getElementById("zonasRiesgoMap").style.display= "none";
-}
--- JS/zonasRiesgoMap.js
+++ JS/zonasRiesgoMap.js
@@ -12,23 +12,21 @@
  marker.bindPopup("<b>Universidad Nacional Mayor de San Marcos</b><br>Ciudad Universitaria, Lima, Perú").openPopup();
 
  let puerta_1 = L.circle([-12.061466, -77.085713], {radius: 20, color: "#ff8000"}).addTo(map);
-    puerta_1.bindPopup(`<h3><b>Puerta 1</b></h3><div>Horas donde más roban</div><ul><li>6:00 p.m. 10 p.m.</li></ul>`)
+    puerta_1.bindPopup(`<h3><b>Puerta 1</b></h3><div>Horas de mayor riesgo</div><ul><li>6:00 p.m. 10 p.m.</li></ul>`)
  let puerta_7 = L.circle([-12.053782, -77.084433], {radius: 20, color: "#ffc100"}).addTo(map);
-    puerta_7.bindPopup(`<h3><b>Puerta 7</b></h3><div>Horas donde más roban</div><ul><li>7 p.m. - 10 p.m.</li></ul>`)
+    puerta_7.bindPopup(`<h3><b>Puerta 7</b></h3><div>Horas de mayor riesgo</div><ul><li>7 p.m. - 10 p.m.</li></ul>`)
  let puerta_8 = L.circle([-12.051823, -77.087571], {radius: 20, color: "#ff8000"}).addTo(map);
-    puerta_8.bindPopup(`<h3><b>Puerta 8</b></h3><div>Horas donde más roban</div><ul><li>8 p.m. - 10 p.m.</li></ul>`)
+    puerta_8.bindPopup(`<h3><b>Puerta 8</b></h3><div>Horas de mayor riesgo</div><ul><li>8 p.m. - 10 p.m.</li></ul>`)
  let puerta_4 = L.circle([-12.055759, -77.080733], {radius: 40, color: "#80ff00"}).addTo(map);
-    puerta_4.bindPopup(`<h3><b>Puerta 4</b></h3><div>Horas donde más roban</div><ul><li>7 p.m. - 10 p.m.</li></ul>`)
+    puerta_4.bindPopup(`<h3><b>Puerta 4</b></h3><div>Horas de mayor riesgo</div><ul><li>7 p.m. - 10 p.m.</li></ul>`)
  let puerta_2 = L.circle([-12.059459, -77.079179], {radius: 15, color: "#ffc100"}).addTo(map);
-    puerta_2.bindPopup(`<h3><b>Puerta 2</b></h3><div>Horas donde más roban</div><ul><li>12 p.m. - 2 p.m.</li><li>7 p.m. - 10 p.m.</li></ul>`)
+    puerta_2.bindPopup(`<h3><b>Puerta 2</b></h3><div>Horas de mayor riesgo</div><ul><li>12 p.m. - 2 p.m.</li><li>7 p.m. - 10 p.m.</li></ul>`)
  let bajo_puente = L.circle([-12.060256, -77.079252], {radius: 15, color: "#ff0000"}).addTo(map);
-    bajo_puente.bindPopup(`<h3><b>Bajo el puente</b></h3><div>Horas donde más roban</div><ul><li>7 p.m. - 10 p.m.</li></ul>`)
+    bajo_puente.bindPopup(`<h3><b>Bajo el puente</b></h3><div>Horas de mayor riesgo</div><ul><li>7 p.m. - 10 p.m.</li></ul>`)
  let puerta_7_parque = L.circle([-12.051846, -77.084210], {radius: 100, color: "red"}).addTo(map);
-    puerta_7_parque.bindPopup(`<h3><b>Parque puerta 7</b></h3><div>Horas donde más roban</div><ul><li>7 p.m. - 8 p.m.</li></ul>`)
+    puerta_7_parque.bindPopup(`<h3><b>Parque puerta 7</b></h3><div>Horas de mayor riesgo</div><ul><li>7 p.m. - 8 p.m.</li></ul>`)
 
     
- function zonasEvent(e){
-    //alert("latitud:"  + e.latlng.lat+", longitud: "+e.latlng.lng);
+ function centrar(e){
+    map.fitBounds([[-12.051823, -77.087571], [-12.060256, -77.079252]]);
  }
-
-map.on("click", zonasEvent);
